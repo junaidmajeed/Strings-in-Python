f696548d4c98a2d5ec2c636bdc598497a49778e6
junaidmajeed <35539538+junaidmajeed@users.noreply.github.com>
2020-11-19 19:29:20 +0500

    Add files via upload

---
+++ strings.py
@@ -0,0 +1,23 @@
+"""
+Examples to show how strings works in python
+
+Sequence of characters
+Contain a-z, 0-9, Special characters
+In double or single quotes
+"""
+
+a = "This is a simple string"
+b = 'Using a single quotes'
+
+print(a)
+print(b)
+
+c = "Need to use 'quotes' inside a string"
+print(c)
+
+d="another way to handle \"double\" quotes"
+print(d)
+
+e = "Bigger string\
+ length"
+print(e)
